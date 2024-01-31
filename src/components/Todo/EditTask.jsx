b0d5399d1057945bdb4e4a37e2a4df60b0af46da
ChaniRoz <m05276539232@GMAIL.COM>
2024-01-31 18:09:28 +0200

    4- ToDo-Slice-Finish-The-addition

--- src/components/Todo/EditTask.jsx
+++ src/components/Todo/EditTask.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -9,8 +10,15 @@ import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
 import RecipeReviewCard from './DrowSingleTask'
 
-export default function EditTaskDialog() {
+export default function EditTaskDialog(props) {
   const [open, setOpen] = React.useState(false);
+  // //
+  // const Tasks = useSelector((state) => state.TaskSlice.Task);
+  // const [id, setId] = React.useState("");
+  // const [name, setName] = React.useState("");
+  // const [createDate, setCreateDate] = React.useState("");
+  // const [checkbox, setCheckbox] = React.useState(false);
+  // //
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -18,6 +26,9 @@ export default function EditTaskDialog() {
 
   const handleClose = () => {
     <RecipeReviewCard/>
+    // debugger
+    // alert(Tasks.name)
+
     setOpen(false);
   };
 
@@ -39,6 +50,14 @@ export default function EditTaskDialog() {
             type="text"
             fullWidth
             variant="standard"
+            // // 
+            // onChange={(e) => {    
+            //   setName(e.target.value)
+            //   var today = new Date(),
+            //     date = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate();
+            //   setCreateDate(date)
+            // }}
+            // // 
           />
         </DialogContent>
         <DialogActions>
